1a059562f082c762775720423a2fe734e6c6275c
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-15 23:25:58 +0330

    import the Component to the App

--- css-frameworks/src/App.jsx
+++ css-frameworks/src/App.jsx
@@ -1,7 +1,9 @@
+import MyButtons from './components/1-AlignedButtons/AlignedButtons.jsx';
 export default function App() {
   return (
     <div>
     hi       
+    <MyButtons />
     </div>
   );
 }
