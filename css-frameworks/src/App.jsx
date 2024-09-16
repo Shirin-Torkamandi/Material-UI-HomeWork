b4765e4a48b122b6550a07ef3dffd63e50a70771
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-16 18:41:36 +0330

    import RegisterForm into App

--- css-frameworks/src/App.jsx
+++ css-frameworks/src/App.jsx
@@ -1,9 +1,11 @@
 import MyButtons from './components/1-AlignedButtons/AlignedButtons.jsx';
+import RegisterForm from './components/2-GridTextFields/GridTextFields.jsx';
 export default function App() {
   return (
     <div>
     hi       
     <MyButtons />
+    <RegisterForm />
     </div>
   );
 }
