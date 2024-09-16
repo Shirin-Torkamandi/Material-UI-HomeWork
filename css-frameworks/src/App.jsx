803b9e41dbd58411bf606adab7260c5f360bb5e0
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-16 19:34:00 +0330

    delete imports in App and move it to the ThreeInOne component

--- css-frameworks/src/App.jsx
+++ css-frameworks/src/App.jsx
@@ -1,11 +1,9 @@
-import MyButtons from './components/1-AlignedButtons/AlignedButtons.jsx';
-import RegisterForm from './components/2-GridTextFields/GridTextFields.jsx';
+import ThreeInOne from './components/3-customTheme/ThreeInOne.jsx';
 export default function App() {
   return (
     <div>
     hi       
-    <MyButtons />
-    <RegisterForm />
+    <ThreeInOne />
     </div>
   );
 }
